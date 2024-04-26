115c58ea93a6d917952351c047aeb798233ffef9
jschilcher <83989369+jschilcher@users.noreply.github.com>
2024-04-26 15:50:30 -0500

    Add components, and axio install

--- jenelle.io-app/src/components/App.js
+++ jenelle.io-app/src/components/App.js
@@ -1,32 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import axios from "axios";
 import './App.css';
 import Title from "./Title/title";
 import Logo from './Logo/logo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <Logo/>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teamData: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getTeamData();
+  }
+
+  async getTeamData() {
+    let response = await axios.get(
+      "http://localhost:5000/api/songs"
+    );
+    this.setState({
+      teamData: response.data,
+    });
+  }
+
+  render() {
+    if (this.state.teamData[0] === undefined) {
+      return <div></div>;
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <div>
+              <Logo/>
+            </div>
+            <div>
+              <Title/>
+            </div>
+            <p>
+              Learn all about your favorite MLB team
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
         </div>
-        <div>
-          <Title/>
-        </div>
-        <p>
-          Learn all about your favorite MLB team
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      );
+    }
+  }
+
+
 }
 
 export default App;
